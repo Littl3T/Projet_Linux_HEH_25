5a34567222cf87fa38efaa10c43297cb18185409
parasol-ka <ananastya.chau@gmail.com>
2025-05-14 11:45:53 +0200

    fix: posibillity to add-delete-restore and delete again without bugs of tar

--- scripts/utils_delete_user.sh
+++ scripts/utils_delete_user.sh
@@ -28,6 +28,7 @@ SQL_DUMP_NAME="${USERNAME}_${DATE}.sql.gz"
 
 # === Creation du dossier archive ===
 echo "📁 Création du dossier $ARCHIVE_DIR sur le serveur de backup si nécessaire..."
+
 mkdir -p $ARCHIVE_DIR
 sudo chown -R backup:backup $ARCHIVE_DIR
 
@@ -35,6 +36,7 @@ sudo chown -R backup:backup $ARCHIVE_DIR
 # === Étapes sur le serveur Web ===
 echo "📡 Suppression de $USERNAME sur $WEB_PRIVATE_IP..."
 ssh -i "$SSH_KEY" ec2-user@$WEB_PRIVATE_IP bash -s <<EOF
+sudo rm -f "/tmp/$ARCHIVE_NAME"
 sudo tar czf "/tmp/$ARCHIVE_NAME" "/srv/www/$USERNAME"
 sudo smbpasswd -x "$USERNAME" || true
 sudo userdel -r "$USERNAME" || true
@@ -45,9 +47,8 @@ EOF
 # === Transfert de l'archive web vers serveur backup ===
 echo "📦 Transfert de l’archive web vers $BACKUP_HOSTNAME..."
 scp -i "$SSH_KEY" ec2-user@$WEB_PRIVATE_IP:/tmp/$ARCHIVE_NAME $REMOTE_USER@$BACKUP_HOSTNAME:$ARCHIVE_DIR/
-
 # === Suppression de l’archive temporaire côté Web ===
-ssh -i "$SSH_KEY" ec2-user@$WEB_PRIVATE_IP "sudo rm -f /tmp/$ARCHIVE_NAME"
+ssh -i "$SSH_KEY" ec2-user@$WEB_PRIVATE_IP "sudo rm -rf /tmp/$ARCHIVE_NAME"
 
 # === Export SQL depuis le backend ===
 ssh -i "$SSH_KEY" ec2-user@$BACKEND_PRIVATE_IP bash -s <<EOF
@@ -65,7 +66,7 @@ EOF
 scp -i "$SSH_KEY" ec2-user@$BACKEND_PRIVATE_IP:/tmp/$SQL_DUMP_NAME $REMOTE_USER@$BACKUP_HOSTNAME:$ARCHIVE_DIR/
 
 # === Suppression du fichier temporaire SQL ===
-ssh -i "$SSH_KEY" ec2-user@$BACKEND_PRIVATE_IP "rm -f /tmp/$SQL_DUMP_NAME"
+ssh -i "$SSH_KEY" ec2-user@$BACKEND_PRIVATE_IP "rm -rf /tmp/$SQL_DUMP_NAME"
 
 # === Suppression DNS ===
 echo "🧹 Suppression de l'entrée DNS..."
